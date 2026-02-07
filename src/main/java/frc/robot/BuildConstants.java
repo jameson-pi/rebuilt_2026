2f777f8286eabf06cf7e372bc9d6462788cc9047
Vaedz7 <75952433+Vaedz7@users.noreply.github.com>
2026-02-07 14:50:10 -0600

    Adjust gravity constant in trajectory simulation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt_2026-1";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 31;
-    public static final String GIT_SHA = "d60f013c6c5e9873ad98b97b58a8a8ee673846ba";
-    public static final String GIT_DATE = "2026-02-07 12:46:07 EST";
+    public static final int GIT_REVISION = 32;
+    public static final String GIT_SHA = "ab9e08ee6f67ec63995a067004b577c1dc6c4cd0";
+    public static final String GIT_DATE = "2026-02-07 13:11:43 EST";
     public static final String GIT_BRANCH = "shooterTesting";
-    public static final String BUILD_DATE = "2026-02-07 13:09:10 EST";
-    public static final long BUILD_UNIX_TIME = 1770487750292L;
+    public static final String BUILD_DATE = "2026-02-07 15:48:19 EST";
+    public static final long BUILD_UNIX_TIME = 1770497299655L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
