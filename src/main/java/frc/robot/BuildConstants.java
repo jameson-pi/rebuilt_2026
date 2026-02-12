0a30d72c0a1c92fb225be8a7b23bc68bdead80a5
Aku Anoop <akshithr@Howdybots.org>
2026-02-11 19:03:38 -0600

    Refactor Indexer subsystem: update BuildConstants, add IndexerSubsystem, and modify IndexerConstants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "rebuilt_2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 10;
-    public static final String GIT_SHA = "1d2d6e78a189defcee10551ce78c2e887a442ffb";
-    public static final String GIT_DATE = "2026-02-05 21:53:03 EST";
+    public static final int GIT_REVISION = 11;
+    public static final String GIT_SHA = "17850ab4999cee3809a971b4c4c7a22bc367ce86";
+    public static final String GIT_DATE = "2026-02-07 15:16:43 EST";
     public static final String GIT_BRANCH = "IndexerSubsystem";
-    public static final String BUILD_DATE = "2026-02-07 15:16:12 EST";
-    public static final long BUILD_UNIX_TIME = 1770495372289L;
+    public static final String BUILD_DATE = "2026-02-07 15:39:12 EST";
+    public static final long BUILD_UNIX_TIME = 1770496752575L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -2,5 +2,7 @@ package frc.robot.subsystems.indexer;
 
 public class IndexerConstants {
     public static final double kCollectorSpeed = 0.5; // Adjust speed as needed
-    public static final double kFeederSpeed = 0.5;
+    public static final double kCollectorSpeedSpeed = 0.5;
+    public static final double kIndexerMotorID = 2;
+     
 }
