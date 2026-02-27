2bf00c7eeb8e3f233856e57e923fa8ebc6303eb9
Anshu <acharya.anshu@gmail.com>
2026-02-26 19:35:16 -0600

    add some vision stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import com.ctre.phoenix6.SignalLogger;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
@@ -289,4 +290,16 @@ public class RobotContainer {
         Logger.recordOutput("FieldSimulation/RobotPosition", driveSimulation.getSimulatedDriveTrainPose());
         Logger.recordOutput("FieldSimulation/Fuel", SimulatedArena.getInstance().getGamePiecesArrayByType("Fuel"));
     }
+
+    public Command getRobotStartPose(int cameraIndex) {
+        return Commands.runOnce(() -> {
+                    Pose3d cameraPose = vision.getStartingPoseFromCamera(cameraIndex);
+                    // Logger.recordOutput("CameraPose", cameraPose);
+                    if (cameraPose != null) {
+                        drive.setPose(cameraPose.toPose2d());
+                    }
+                })
+                .ignoringDisable(true);
+    }
+
 }
